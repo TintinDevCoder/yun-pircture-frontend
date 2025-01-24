73d6c943557bee9c5340aacc50b2fab33958be60
dd <18211882344@163.com>
2025-01-24 16:50:52 +0800

    初始化前端

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <BasicLayout/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import { healthUsingGet } from '@/api/mainController'
+import {useLoginUserStore} from "@/stores/useLoginUserStore.ts";
+
+healthUsingGet().then((res) => {
+  console.log(res)
+})
+//获取登陆用户信息
+const loginUserStore = useLoginUserStore();
+loginUserStore.fetchLoginUser()
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,95 @@
+<template>
+  <a-row :wrap="false">
+    <a-col flex="200px">
+      <RouterLink to="/">
+        <div class="title-bar">
+          <img class="logo" src="../assets/yoyo.png" alt="logo"/>
+          <div class="title">悠悠云图库</div>
+        </div>
+      </RouterLink>
+    </a-col>
+    <a-col flex="auto">
+      <a-menu
+        v-model:selectedKeys="current"
+        mode="horizontal"
+        :items="items"
+        @click="doMenuClick"
+      />
+    </a-col>
+    <a-col flex="120px">
+      <div class="user-login-status">
+        <div v-if="loginUserStore.loginUser.id">
+          {{ loginUserStore.loginUser.userName ?? '无名' }}
+        </div>
+        <div v-else>
+          <a-button type="primary" href="/user/login">登录</a-button>
+        </div>
+      </div>
+
+    </a-col>
+  </a-row>
+
+</template>
+<script lang="ts" setup>
+import {h, ref} from 'vue'
+import {HomeOutlined} from '@ant-design/icons-vue'
+import {MenuProps} from 'ant-design-vue'
+
+
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/about',
+    label: '关于',
+    title: '关于',
+  },
+  {
+    key: 'others',
+    label: h('a', {href: 'https://www.codefather.cn', target: '_blank'}, '编程导航'),
+    title: '编程导航',
+  },
+])
+
+import {useRouter} from "vue-router";
+import {useLoginUserStore} from "@/stores/useLoginUserStore.ts";
+
+const router = useRouter();
+//当前要高亮的菜单项
+const current = ref<string[]>([])
+// 路由跳转事件
+const doMenuClick = ({key}: { key: string }) => {
+  router.push({
+    path: key,
+  });
+};
+
+// 监听路由变化，更新当前选中菜单
+router.afterEach((to, from, next) => {
+  current.value = [to.path];
+});
+//引入用户状态
+const loginUserStore = useLoginUserStore();
+</script>
+
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  font-size: 18px;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>

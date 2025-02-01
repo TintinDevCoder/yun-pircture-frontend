fe56be65e91f556e8063c2b7e225b1e131008855
dd <18211882344@163.com>
2025-02-01 10:34:45 +0800

    5-修改新增页面

--- page/AddPicturePage.vue
+++ page/AddPicturePage.vue
@@ -51,7 +51,7 @@
         />
       </a-form-item>
       <a-form-item>
-        <a-button type="primary" html-type="submit" style="width: 100%">创建</a-button>
+        <a-button type="primary" html-type="submit" style="width: 100%">{{ route.query?.id ? '修改' : '创建' }}</a-button>
       </a-form-item>
     </a-form>
   </div>

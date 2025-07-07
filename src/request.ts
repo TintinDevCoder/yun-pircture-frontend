5a54551f10a99f496ba04d3c9123a226ab7e21c9
Administrator <18211882344@163.com>
2025-07-07 15:33:14 +0800

    feat(): 新增生产环境配置

--- src/request.ts
+++ src/request.ts
@@ -1,12 +1,16 @@
 import axios from 'axios'
 import { message } from 'ant-design-vue'
 
+// 区分开发和生产环境
+const DEV_BASE_URL = "http://localhost:8123";
+//const PROD_BASE_URL = "http://101.43.156.106:8123";
+const PROD_BASE_URL = "http://www.ddcoding.work";
 // 创建 Axios 实例
 const myAxios = axios.create({
-  baseURL: 'http://localhost:8123',
-  timeout: 60000,
+  baseURL: PROD_BASE_URL,
+  timeout: 10000,
   withCredentials: true,
-})
+});
 
 // 全局请求拦截器
 myAxios.interceptors.request.use(
